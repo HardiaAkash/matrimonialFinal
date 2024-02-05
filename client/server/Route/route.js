ee623ae9db50eff01aeb0f2ae746aa98bc016904
Akash Hardia <113900374+HardiaAkash@users.noreply.github.com>
2024-02-05 18:44:23 +0530

    SuperAdmin

--- client/server/Route/route.js
+++ client/server/Route/route.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const multer = require('multer');
 const { authorizeRoles, isAuthJWT } = require("../Utils/jwt");
-const { addAdmin, adminLogin, adminLogout, changeAdminPwd } = require("../Controller/AdminAuth");
+const { addAdmin, adminLogin, adminLogout, changeAdminPwd, getAllAdmins, deleteAdmin } = require("../Controller/AdminAuth");
 const { addUser, userLogin, deleteUser, updateUser, logoutUser, getUserByID, viewUser, forgotPwd, resetPassword, changeUserPwd, verifyUser, deleteUserReq, getDeleteUserRequests } = require("../Controller/UserAuth");
 const { addForm, editFormById, changeStatusForm, uploadImage, viewForm, deleteFormById, getFormByUserID, changeMatchStatus, approvedForm, getFormById, findPotentialPartners } = require("../Controller/UserFormAuth");
 const { addCounVideo, getAllCounsVideo } = require("../Controller/VideoAuth");
@@ -10,39 +10,41 @@ const { addUserOTP } = require("../Controller/OtpAuth");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.route("/addAdmin").post(isAuthJWT, authorizeRoles("Admin"),addAdmin)
+router.route("/addAdmin").post(isAuthJWT, authorizeRoles("SuperAdmin"),addAdmin)
+router.route("/viewAdmin").get(isAuthJWT, authorizeRoles("SuperAdmin"), getAllAdmins)
+router.route("/deleteAdmin/:id").get(isAuthJWT,authorizeRoles("SuperAdmin"),deleteAdmin)
 router.route("/adminlogin").post(adminLogin)
-router.route("/logoutAdmin").get(isAuthJWT, authorizeRoles("Admin"),adminLogout)
-router.route("/changeadminpassword").post(isAuthJWT,authorizeRoles("Admin"),changeAdminPwd)
+router.route("/logoutAdmin").get(isAuthJWT, authorizeRoles("Admin","SuperAdmin"),adminLogout)
+router.route("/changeadminpassword").post(isAuthJWT,authorizeRoles("Admin","SuperAdmin"),changeAdminPwd)
 ///////////////user/////////////
 router.route("/adduser").post(addUser)
 router.route("/generateOTP").post(addUserOTP)
 router.route("/userlogin").post(userLogin)
-router.route("/deleteUser/:id").delete(isAuthJWT,authorizeRoles("Admin"),deleteUser)
+router.route("/deleteUser/:id").delete(isAuthJWT,authorizeRoles("Admin","SuperAdmin"),deleteUser)
 router.route("/updateUser").put(isAuthJWT,updateUser)
 router.route("/logoutUser").get(isAuthJWT, logoutUser)
 router.route("/getuser/:id").get(isAuthJWT,getUserByID)
-router.route("/viewUser").get(isAuthJWT,authorizeRoles("Admin"),viewUser)
+router.route("/viewUser").get(isAuthJWT,authorizeRoles("Admin","SuperAdmin"),viewUser)
 router.route("/forgotpassword").post(forgotPwd)
 router.route("/resetpassword").post(resetPassword)
 router.route("/changeUserPassword").post(isAuthJWT,changeUserPwd)
 router.route("/verifyTokenUser/:token").get(verifyUser)
 router.route("/deleteUserReq").post(isAuthJWT, deleteUserReq)
-router.route("/getDeleteReq").get(isAuthJWT, authorizeRoles("Admin"),getDeleteUserRequests)
+router.route("/getDeleteReq").get(isAuthJWT, authorizeRoles("Admin","SuperAdmin"),getDeleteUserRequests)
 
 ////////////////////////////form/////////
 router.route("/uploadImage").post(isAuthJWT, upload.single('file'),uploadImage)
 router.route("/addForm").post(isAuthJWT, addForm)
-router.route("/deleteForm/:id").delete(isAuthJWT, authorizeRoles("Admin"),deleteFormById)
+router.route("/deleteForm/:id").delete(isAuthJWT, authorizeRoles("Admin","SuperAdmin"),deleteFormById)
 router.route("/editForm/:id").put(isAuthJWT, editFormById)
-router.route("/changeStatus/:id").put(isAuthJWT, authorizeRoles("Admin"),changeStatusForm)
-router.route("/viewForm").get(isAuthJWT,authorizeRoles("Admin"),viewForm)
+router.route("/changeStatus/:id").put(isAuthJWT, authorizeRoles("Admin","SuperAdmin"),changeStatusForm)
+router.route("/viewForm").get(isAuthJWT,authorizeRoles("Admin","SuperAdmin"),viewForm)
 router.route("/getFormByUser").post(isAuthJWT,getFormByUserID)
-router.route("/getFormByID/:id").get(isAuthJWT,authorizeRoles("Admin"),getFormById)
-router.route("/counselVideo").post(isAuthJWT,authorizeRoles("Admin"),addCounVideo)
+router.route("/getFormByID/:id").get(isAuthJWT,authorizeRoles("Admin","SuperAdmin"),getFormById)
+router.route("/counselVideo").post(isAuthJWT,authorizeRoles("Admin","SuperAdmin"),addCounVideo)
 router.route("/getCounselVideo").get(isAuthJWT, getAllCounsVideo)
-router.route("/isMatched/:id").post(isAuthJWT, authorizeRoles("Admin"),changeMatchStatus)
-router.route("/approvedForm").get(isAuthJWT, authorizeRoles("Admin"), approvedForm)
+router.route("/isMatched/:id").post(isAuthJWT, authorizeRoles("Admin","SuperAdmin"),changeMatchStatus)
+router.route("/approvedForm").get(isAuthJWT, authorizeRoles("Admin","SuperAdmin"), approvedForm)
 router.route("/getTest/:id").get(findPotentialPartners)
 router.route("/getPotentialPartner/:id").get(isAuthJWT,findPotentialPartners)
 module.exports = router;
